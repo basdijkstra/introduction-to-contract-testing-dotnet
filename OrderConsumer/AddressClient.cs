e40c0f47056385a6e017332522b0da42057e6e76
Bas Dijkstra <bas@ontestautomation.com>
2023-09-22 16:03:35 +0200

    Add order consumer

---
+++ OrderConsumer/AddressClient.cs
@@ -0,0 +1,44 @@
+﻿namespace OrderConsumer
+{
+    public class AddressClient
+    {
+        private readonly Uri baseUri;
+
+        public AddressClient(Uri baseUri)
+        {
+            this.baseUri = baseUri;
+        }
+
+        public async Task<HttpResponseMessage> GetAddress(string id)
+        {
+            using (var client = new HttpClient { BaseAddress = baseUri })
+            {
+                try
+                {
+                    var response = await client.GetAsync($"/address/{id}");
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("There was a problem connecting to the AddressProvider API.", ex);
+                }
+            }
+        }
+
+        public async Task<HttpResponseMessage> DeleteAddress(string id)
+        {
+            using (var client = new HttpClient { BaseAddress = baseUri })
+            {
+                try
+                {
+                    var response = await client.DeleteAsync($"/address/{id}");
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("There was a problem connecting to the AddressProvider API.", ex);
+                }
+            }
+        }
+    }
+}
