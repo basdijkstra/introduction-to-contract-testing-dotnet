94eee1bf7616d3d736484b635b261ea72f1b8527
Bas Dijkstra <bas@ontestautomation.com>
2023-09-22 15:59:40 +0200

    Extend tests for customer_consumer

---
+++ CustomerConsumer.Tests/AddressClientDeleteTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System.Net;
+
+namespace CustomerConsumer.Tests
+{
+    [TestFixture]
+    public class AddressClientDeleteTests : PactTestBase
+    {
+        [Test]
+        public async Task DeleteAddress_AddressIdIsValid()
+        {
+            pact.UponReceiving("A request to delete an address")
+                    .Given($"address ID invalid_address_id is valid")
+                    .WithRequest(HttpMethod.Delete, $"/address/{addressId}")
+                .WillRespond()
+                    .WithStatus(HttpStatusCode.NoContent);
+
+            await pact.VerifyAsync(async ctx => {
+                var response = await client.DeleteAddress(addressId);
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
+            });
+        }
+
+        [Test]
+        public async Task DeleteAddress_AddressIdIsInvalid()
+        {
+            pact.UponReceiving("A request to delete an address")
+                    .Given($"address ID invalid_address_id is invalid")
+                    .WithRequest(HttpMethod.Delete, "/address/invalid_address_id")
+                .WillRespond()
+                    .WithStatus(HttpStatusCode.BadRequest);
+
+            await pact.VerifyAsync(async ctx => {
+                var response = await client.DeleteAddress("invalid_address_id");
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+            });
+        }
+    }
+}
--- CustomerConsumer/AddressClient.cs
+++ CustomerConsumer/AddressClient.cs
@@ -24,5 +24,21 @@
                 }
             }
         }
+
+        public async Task<HttpResponseMessage> DeleteAddress(string id)
+        {
+            using (var client = new HttpClient { BaseAddress = baseUri })
+            {
+                try
+                {
+                    var response = await client.DeleteAsync($"/address/{id}");
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("There was a problem connecting to the AddressProvider API.", ex);
+                }
+            }
+        }
     }
 }
