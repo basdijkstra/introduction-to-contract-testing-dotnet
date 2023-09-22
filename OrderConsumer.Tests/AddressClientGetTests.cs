c0cbb841184a7c5166b3eea33fa17e0e677a2bbe
Bas Dijkstra <bas@ontestautomation.com>
2023-09-22 16:15:42 +0200

    Add order_consumer test project

---
+++ OrderConsumer.Tests/AddressClientGetTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System.Net;
+
+namespace OrderConsumer.Tests
+{
+    [TestFixture]
+    public class AddressClientGetTests : PactTestBase
+    {
+        [Test]
+        public async Task GetAddress_AddressIdExists()
+        {
+            pact.UponReceiving("A request to retrieve an address")
+                    .Given($"address with ID {addressId} exists")
+                    .WithRequest(HttpMethod.Get, $"/address/{addressId}")
+                .WillRespond()
+                    .WithStatus(HttpStatusCode.OK)
+                    .WithHeader("Content-Type", "application/json; charset=utf-8")
+                    .WithJsonBody(address);
+
+            await pact.VerifyAsync(async ctx => {
+                var response = await client.GetAddress(addressId);
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            });
+        }
+
+        [Test]
+        public async Task GetAddress_AddressIdDoesNotExist()
+        {
+            pact.UponReceiving("A request to retrieve an address")
+                    .Given($"address with ID {addressId} does not exist")
+                    .WithRequest(HttpMethod.Get, $"/address/{addressId}")
+                .WillRespond()
+                    .WithStatus(HttpStatusCode.NotFound);
+
+            await pact.VerifyAsync(async ctx => {
+                var response = await client.GetAddress(addressId);
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+            });
+        }
+
+        [Test]
+        public async Task GetAddress_AddressIdIsInvalid()
+        {
+            pact.UponReceiving("A request to retrieve an address")
+                    .Given($"address ID invalid_address_id is invalid")
+                    .WithRequest(HttpMethod.Get, "/address/invalid_address_id")
+                .WillRespond()
+                    .WithStatus(HttpStatusCode.BadRequest);
+
+            await pact.VerifyAsync(async ctx => {
+                var response = await client.GetAddress("invalid_address_id");
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+            });
+        }
+    }
+}
---
+++ OrderConsumer.Tests/PactTestBase.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using PactNet;
+using PactNet.Matchers;
+
+namespace OrderConsumer.Tests
+{
+    public class PactTestBase
+    {
+        protected IPactBuilderV3 pact;
+        protected AddressClient client;
+        protected object address;
+        protected readonly string addressId = "93edc1a1-5093-4d30-a9c1-da04765553b7";
+
+        private string pactDir = Path.Join("..", "..", "..", "pacts");
+        private readonly int port = 9876;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var Config = new PactConfig
+            {
+                PactDir = pactDir,
+                DefaultJsonSettings = new JsonSerializerSettings()
+            };
+
+            pact = Pact.V3("order_consumer", "address_provider", Config).WithHttpInteractions(port);
+            client = new AddressClient(new Uri($"http://localhost:{port}"));
+
+            address = new
+            {
+                Id = Match.Regex(addressId, "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"),
+                AddressType = Match.Type("billing"),
+                Street = Match.Type("Main street"),
+                Number = Match.Integer(123),
+                City = Match.Type("Los Angeles"),
+                ZipCode = Match.Integer(12345),
+                State = Match.Type("California"),
+                Country = Match.Type("United States")
+            };
+        }
+    }
+}
